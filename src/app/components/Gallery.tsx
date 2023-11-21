5eadac2647cda695b45dbee6cf1d45774d7af584
diopmorfall <morfall.papydiop99@gmail.com>
2023-11-21 15:35:48 +0000

    Responsive image optimization

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -8,11 +8,10 @@ export default async function Gallery(){
     if(!images) return 
         <h2 className="m-4 text-2xl font-bold">No images found</h2>
 
-
     return(
         <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
             {images.photos.map(photo => (
-                <ImgContainer photo={photo} />
+                <ImgContainer key={photo.id} photo={photo} />
             ))}
         </section>
     )
