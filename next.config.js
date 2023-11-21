10e6053008dc3bd2f1d57f0e794849c45ea88df4
diopmorfall <morfall.papydiop99@gmail.com>
2023-11-21 15:04:08 +0000

    Remote images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: { //* this configuration is needed when we use remote urls; we destructure  it here
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/photos/**'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,6 @@
 import type { ImagesResults } from "@/models/Images";
 import { fetchImages } from "@/lib/fetchImages";
-import { PHASE_PRODUCTION_BUILD } from "next/dist/shared/lib/constants";
+import ImgContainer from './ImgContainer';
 
 export default async function Gallery(){
     const url = "https://api.pexels.com/v1/curated?"
@@ -12,7 +12,7 @@ export default async function Gallery(){
     return(
         <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
             {images.photos.map(photo => (
-                <div key={photo.id} className="h-64 bg-gray-200 rounded-xl"></div>
+                <ImgContainer photo={photo} />
             ))}
         </section>
     )
